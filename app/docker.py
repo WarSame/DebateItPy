68d6eccbf74a05f89f9fda7e5bd8b2c8a17a34c3
WarSame <graemecliffe@gmail.com>
2018-04-09 18:41:42 -0400

    Remove port and flask_env environment variables.

--- app/docker.py
+++ app/docker.py
@@ -3,6 +3,4 @@ import os
 
 def info():
     debug = "DEBUG is {}".format( os.environ.get( "DEBUG" ) )
-    flask_env = "FLASK_ENV is {}".format( os.environ.get( "FLASK_ENV" ) )
-    port = "PORT is {}".format( os.environ.get( "PORT" ) )
-    return debug + "\n" + flask_env + "\n" + port
+    return debug
