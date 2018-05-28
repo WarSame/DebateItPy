1d2b00c523645c79e1812f260f5d8ae799f3b896
WarSame <graemecliffe@gmail.com>
2018-05-27 21:53:05 -0400

    Basic templating

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,16 @@ from flask import render_template
 
 
 @app.route("/")
-def thisfunc():
+def index():
     return render_template("index.html")
+
+
+@app.route("/u/<username>")
+def print_user(username):
+    return username
+
+
+@app.route("/c/<community_name>")
+def print_community(community_name):
+    return community_name
 
