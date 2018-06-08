ffcc3728baf839eed0c4fef1411b2f2f5f436bdb
WarSame <graemecliffe@gmail.com>
2018-06-07 22:05:01 -0400

    Working Redis counter

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
+import redis
+
+db = redis.Redis(host="redis", port=6379)
+db.set("count", 0)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    db.incr("count", 1)
+    return render_template("index.html", count=db.get("count"))
 
 
 @app.route("/u/<username>")
