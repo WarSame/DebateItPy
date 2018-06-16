08b2a7fc7a286711e6cf543858fde6aa138474df
WarSame <graemecliffe@gmail.com>
2018-06-16 11:10:09 -0400

    Start of secrets

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ import os
 
 class Config(object):
     DEBUG = os.environ.get("FLASK_DEBUG") or False
-    SECRET_KEY = "somesecretkey"
+    SECRET_KEY = os.environ.get("FORM_SECRET_KEY") or ""
+    GOOGLE_LOGIN_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") or ""
+    GOOGLE_LOGIN_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET") or ""
+    FACEBOOK_LOGIN_CLIENT_ID = os.environ.get("FACEBOOK_CLIENT_ID") or ""
+    FACEBOOK_LOGIN_CLIENT_SECRET = os.environ.get("FACEBOOK_CLIENT_SECRET") or ""
