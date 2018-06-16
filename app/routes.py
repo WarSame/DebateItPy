b342cad2512436937c6e2f5e7eadda1cde571272
WarSame <graemecliffe@gmail.com>
2018-06-16 14:27:56 -0400

    Clean up of oauth, still not fully working

--- app/routes.py
+++ app/routes.py
@@ -4,19 +4,16 @@ from .forms import EmailPasswordForm
 import redis
 
 db = redis.Redis(host="redis", port=6379)
-db.set("count", 0)
 
 
 @app.route("/")
 def index():
-    db.incr("count", 1)
-    count = db.get("count")
     user = db.get("user")
     if user is None:
         user = ""
     else:
         user = user.decode("utf-8")
-    return render_template("index.html", count=int(count), user=user)
+    return render_template("index.html", user=user)
 
 
 @app.route("/u/<username>")
@@ -27,14 +24,6 @@ def print_user(username):
 @app.route("/c/<community_name>")
 def print_community(community_name):
     return community_name
-
-
-@app.route("/submit", methods=("GET", "POST"))
-def submit():
-    form = MyForm()
-    if form.validate_on_submit():
-        return redirect("/")
-    return render_template("login.html", form=form)
 
 
 @app.route("/login", methods=("GET", "POST"))
