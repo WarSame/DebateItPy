513c351aab079d04fe60cdb47b403e653de0f808
WarSame <graemecliffe@gmail.com>
2018-06-17 22:55:38 -0400

    Works without primaryjoin

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
-    communities = db.relationship('Community', backref=db.backref('user'), primaryjoin="Community.id == User.community_id")
+    communities = db.relationship('Community', backref=db.backref('user'))
 
     def __repr__(self):
         return "<User: {0}>".format(self.name)
@@ -26,7 +26,7 @@ class Post(db.Model):
     text = db.Column(db.String(100000), nullable=False)
     create_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('post'), uselist=False, primaryjoin="Post.user_id == User.id")
+    user = db.relationship('User', backref=db.backref('post'), uselist=False)
 
     def __repr__(self):
         return "<Post: {0}>".format(self.title)
