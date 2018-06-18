772357462c88b3ce57990e8f154b9f8a85cd4630
WarSame <graemecliffe@gmail.com>
2018-06-17 22:14:06 -0400

    Work on models, add post and community

--- app/models.py
+++ app/models.py
@@ -1,13 +1,41 @@
 from .db import db
+from datetime import datetime
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False, primary_key=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
+    communities = db.relationship('Community', backref=db.backref('user'))
 
     def __repr__(self):
-        return "<User: {0}>".format(self.username)
+        return "<User: {0}>".format(self.name)
 
     def __init__(self, name):
-        self.id = id
         self.name = name
+
+
+class Community(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<Community: {0}>".format(self.name)
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000), nullable=False)
+    text = db.Column(db.String(10000), nullable=False)
+    create_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('post'), uselist=False)
+
+    def __repr__(self):
+        return "<Post: {0}>".format(self.title)
+
+    def __init__(self, text):
+        self.text = text
