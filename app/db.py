2359dcc95a39d6614ed9731dbe9b8c635b8e340a
WarSame <graemecliffe@gmail.com>
2018-06-21 20:36:33 -0400

    Get app context properly

--- app/db.py
+++ app/db.py
@@ -6,6 +6,8 @@ db = SQLAlchemy(app)
 
 
 def initialize_db():
-    db.metadata.drop_all(bind=db.engine)
-    db.metadata.create_all(bind=db.engine)
-    db.session.commit()
+    db.init_app(app)
+    with app.app_context():
+        db.metadata.drop_all(bind=db.engine)
+        db.metadata.create_all(bind=db.engine)
+        db.session.commit()
