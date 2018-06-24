d5f6359ea3d9cea4079382d6e280823839cf9d0c
WarSame <graemecliffe@gmail.com>
2018-06-24 15:42:17 -0400

    Fixed db config, need to get attribute db for flask app

--- test/test_models.py
+++ test/test_models.py
@@ -2,11 +2,14 @@ import debateit
 import unittest
 from flask_sqlalchemy import SQLAlchemy
 
+
 class testUser(unittest.TestCase):
     def setUp(self):
         debateit.app.config["SECRET_KEY"] = "this"
+        debateit.app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://pguser:pguser@db:5432/debateit"
+        debateit.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         debateit.testing = True
-        debateit.db = SQLAlchemy(debateit)
+        debateit.db = SQLAlchemy(debateit.app)
         self.app = debateit.app.test_client()
         with debateit.app.app_context():
             debateit.app.db.initialize_db()
