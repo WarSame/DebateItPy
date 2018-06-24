6855315d0ccc1888ab9dac9c431a4031d50a571d
WarSame <graemecliffe@gmail.com>
2018-06-24 16:55:06 -0400

    Working tests on docker-compose up, but run into username non-unique error and web stopping after tests

--- test/test_models.py
+++ test/test_models.py
@@ -1,5 +1,6 @@
 from app import app
 import unittest
+from app.models import User, Community, Debate, Post
 
 
 class testUser(unittest.TestCase):
@@ -7,8 +8,11 @@ class testUser(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def test(self):
-        print("hello")
+    def test_user(self):
+        User.create(name="this", email="that@that.com")
+
+    def test_community(self):
+        Community.create(name="mycommunity")
 
 
 if __name__ == "__main__":
