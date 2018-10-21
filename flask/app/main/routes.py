1f624f6835a4156e9a4354e8c389c3b38a78b3ad
WarSame <graemecliffe@gmail.com>
2018-10-20 20:07:06 -0400

    Untested fix of other routes and list top debates

--- flask/app/main/routes.py
+++ flask/app/main/routes.py
@@ -9,26 +9,24 @@ from app.views import CommunitySchema, UserSchema
 
 @bp.route("/api/u/", methods=["POST"])
 def create_user(user_id=None):
-    if request.method == "POST":
-        user = User.create(**request.get_json)
-        return 200
-    user = User.retrieve_one(id=user_id)
-    current_app.logger.info(user)
-    if user is None:
-        return abort(404)
-    return user
+    json = request.get_json()
+    current_app.logger.info(json)
+    if User.retrieve_one(username=json["username"]) is not None:
+        return jsonify(success=False, reason='User with that username already exists')
+    user = User.create(**json)
+    user_json = UserSchema().dump(result).data
+    return jsonify(user_json)
 
 
 @bp.route("/api/u/<user_id>", methods=["GET"])
 def get_user(user_id=None):
-    if request.method == "POST":
-        user = User.create(**request.get_json)
-        return 200
     user = User.retrieve_one(id=user_id)
     current_app.logger.info(user)
     if user is None:
         return abort(404)
-    return user
+    user_json = UserSchema().dump(user).data
+    current_app.logger.info(user_json)
+    return jsonify(user_json)
 
 
 @bp.route("/api/c/<community_id>", methods=["GET"])
@@ -42,46 +40,57 @@ def get_specific_community(community_id=None):
     return jsonify(community_json)
 
 
-@bp.route("/api/c", methods=["GET"])
-def get_all_communities():
-    return None
-
-
 @bp.route("/api/c", methods=["POST"])
 def create_community():
     json = request.get_json()
     current_app.logger.info(json)
     if Community.retrieve_one(name=json["name"]) is not None:
-        return jsonify(success=False, reason='Community already exists')
-    result = Community.create(**json)
-    result_json = CommunitySchema().dump(result).data
-    return jsonify(result_json)
+        return jsonify(success=False, reason='Community with that name already exists')
+    community = Community.create(**json)
+    community_json = CommunitySchema().dump(community).data
+    return jsonify(community_json)
 
 
-@bp.route("/api/d/", methods=["POST"])
 @bp.route("/api/d/<debate_id>", methods=["GET"])
-def debate(debate_id=None):
-    if request.method == "POST":
-        Debate.create(**request.get_json)
-        return 200
-    debate=Debate.retrieve_one(id=debate_id)
+def get_debate(debate_id=None):
+    debate = Debate.retrieve_one(id=debate_id)
     current_app.logger.info(debate)
     if debate is None:
         return abort(404)
-    return debate
+    debate_json = DebateSchema().dump(debate).data
+    current_app.logger.info(debate_json)
+    return jsonify(debate_json)
 
 
-@bp.route("/api/p/", methods=["POST"])
+@bp.route("/api/d/", methods=["POST"])
+def create_debate():
+    json = request.get_json()
+    current_app.logger.info(json)
+    if Debate.retrieve_one(name=json["name"]) is not None:
+        return jsonify(success=False, reason='Debate with that name already exists')
+    debate = Debate.create(**json)
+    debate_json = DebateSchema().dump(debate).data
+    return jsonify(debate_json)
+
+
 @bp.route("/api/p/<post_id>", methods=["GET"])
-def post(post_id=None):
-    if request.method == "POST":
-        post = Post.create(**request.get_json)
-        return 200
+def get_post(post_id=None):
     post = Post.retrieve_one(id=post_id)
     current_app.logger.info(post)
     if post is None:
         return abort(404)
-    return post
+    post_json = PostSchema().dump(post).data
+    current_app.logger.info(post_json)
+    return jsonify(post_json)
+
+
+@bp.route("/api/p/", methods=["POST"])
+def create_post():
+    json = request.get_json()
+    current_app.logger.info(json)
+    post = Post.create(**json)
+    post_json = PostSchema().dump(post).data
+    return jsonify(post_json)
 
 
 @bp.route("/api/top/d/<count>", methods=["GET"])
@@ -89,12 +98,5 @@ def top_debates(count=0):
     current_app.logger.info("Retrieving top " + count + " rows")
     top_debates = Debate.retrieve_some(count)
     current_app.logger.info("Retrieved {} rows".format(len(top_debates)))
-    top_debates_json = []
-    for d in top_debates:
-        debate = dict()
-        debate["title"] = d.title
-        debate["text"] = d.text
-        debate["id"] = d.id
-        debate["description"] = d.description
-        top_debates_json.append(debate)
+    top_debates_json = UserSchema(many=True).dump(top_debates).data
     return jsonify(top_debates_json)
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,5 @@
 from app import ma
-from app.models import Community, User
+from app.models import Community, User, Debate, Post
 
 class CommunitySchema(ma.ModelSchema):
     class Meta:
@@ -8,4 +8,15 @@ class CommunitySchema(ma.ModelSchema):
 
 class UserSchema(ma.ModelSchema):
     class Meta:
+        fields = ('name', 'id')
         model = User
+
+class DebateSchema(ma.ModelSchema):
+    class Meta:
+        fields = ('title', 'text', 'description', 'creator_id', 'community_id')
+        model = Debate
+
+class PostSchema(ma.ModelSchema):
+    class Meta:
+        fields = ('title', 'text', 'user_id', 'debate_id')
+        model = Post
